24df6a6cf80344f85c9dafb759c0263e151ddf5d
Ashish Nimonkar <ashishjn@udel.edu>
2018-09-01 19:50:09 +0000

    changed the code

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+
 using namespace std;
 
-int main(){
-  string text = "Andy is the greatest!";
-  string subst = text.substr(0,7);
-  cout<<subst<<endl;
-  return 0;
+int main() {
+  string text = "text1";
+  string text2 = "text2";
+  if (text.compare(text2) == 0){
+    cout << "They are the same" << endl;
+  } else {
+    cout << "They are NOT the same" << endl;
+  }
 }
